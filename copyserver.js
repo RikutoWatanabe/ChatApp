437d69948f560d3b2b797ebae7e07d8f61b71d69
rikuto <rkt.w2024@outlook.jp>
2016-01-24 03:05:23 +0900

    use express and change index.html

--- copyserver.js
+++ copyserver.js
@@ -2,25 +2,13 @@ var express = require("express");
 var http = require("http");
 var fs = require("fs");
 
-/*
-var server = module.express = express.createServer( function(req , res){
+var server = http.createServer( function(req , res){
 	res.writeHead(200, {"Content-Type" : "text/html"});
 	res.end(fs.readFileSync("./index.html","utf-8"));
-}).listen(process.env.PORT || 8080); 
-*/
-
-var server = express();
-server.set("port" , (process.env.PORT || 8080));
-
+    }).listen(process.env.PORT || 8080);
 console.log("Server listening...");
-console.log("Express server listening on port %d in %s mode", server.address().port, server.settings.env);
 
 var io = require("socket.io").listen(server);
-
-	io.configure(function(){
-    	io.set('transports', ['xhr-polling']);
-    	io.set('polling duration', 10);
-	});
 
 //user collection
 var userHash = {};
@@ -33,23 +21,22 @@ io.sockets.on("connection" , function (socket) {
 		var msg = "[" + name + "] entered.";
 		userHash[socket.id] = name;
 		io.sockets.emit("publish",{value: msg});
-	});
+	    });
 	//send message
 	socket.on("publish" , function (data) {
 		io.sockets.emit("publish", {value:data.value});
-	});
+	    });
 	//change username
 	socket.on("change" , function (user , data){
-			userHash[socket.id] = user;
+		userHash[socket.id] = user;
 		io.sockets.emit("publish",{value:data.value});
-	});
+	    });
 	//disconnect
 	socket.on("disconnect" , function () {
 		if(userHash[socket.id]){
-			var msg = userHash[socket.id] + " exited.";
-			delete userHash[socket.id];
-			io.sockets.emit("publish", {value: msg});
+		    var msg = userHash[socket.id] + " exited.";
+		    delete userHash[socket.id];
+		    io.sockets.emit("publish", {value: msg});
 		}
-	});
-});
-
+	    });
+    });
