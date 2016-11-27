fd455674e903ec3f20445eb40283651f4e515d5a
rikuto <rkt.w2024@outlook.jp>
2016-11-27 15:50:47 +0900

    shsd

--- server.js
+++ server.js
@@ -41,39 +41,31 @@ io.sockets.on("connection" , function (socket) {
 
 	//start conection
 	socket.on("connected", function (name) {
+		var msg = "[" + name + "] entered.";
+		userHash[socket.id] = name;
 		//
 		collection.find(function(err,docs){
 			io.sockets.emit("openmsg",docs);
 		}).limit(10).sort({'date':-1});
 		//
-		var msg = "[" + name + "] entered.";
-		userHash[socket.id] = name;
 		io.sockets.emit("publish",{value: msg});
 	});
 	//send message
 	socket.on("publish" , function (data) {
 		io.sockets.emit("publish", {value:data.value});
-		var date_now = new Date().toLocaleTimeString();
-		console.log(date_now);
-		push = new collection();
-		push.msg = data.value;
-		push.date = date_now;
-		push.save(function (err){  
-		  if(err){
-    		return console.error(err);
-  		}
-  		console.log("data insert");
-  		});
+		//db insert
+		var push = new collection();
+		adddb(data.value,push);
 	});
 	//change username
 	socket.on("change" , function (user , data){
-			userHash[socket.id] = user;
+		userHash[socket.id] = user;
 		io.sockets.emit("publish",{value:data.value});
 	});
 	//disconnect
 	socket.on("disconnect" , function () {
 		if(userHash[socket.id]){
-			var msg = userHash[socket.id] + " exited.";
+			var msg = "[" + userHash[socket.id] + "]" + " exited.";
 			delete userHash[socket.id];
 			io.sockets.emit("publish", {value: msg});
 		}
@@ -88,6 +80,20 @@ var chatschema = mongoose.Schema({
 
 var collection = mongoose.model('mycollections', chatschema);
 
+var options = { weekday: "long", year: "numeric", month: "short",
+    day: "numeric" }
+
+function adddb(msg,collection){
+		collection.msg = msg;
+		collection.date = new Date().toLocaleTimeString("ja-JP",options);
+		collection.save(function (err){  
+		  if(err){
+    		return console.error(err);
+  		}
+  		console.log("data insert");
+  		});
+
+}
 
 //db connection
 mongoose.connection.on("connected", function(){
